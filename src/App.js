dbb23aabfc30f1b235a903656b77e825e107a50e
Andrew Mullins <mullinsand15@gmail.com>
2023-01-29 07:58:16 -0700

    completed lesson with errors present

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Ideas from './Ideas';
+import Form from './Form';
+
+
+const initialIdeas = [
+  { id: 1, title: 'Sweaters for pugs', description: 'Sweaters but only for pugs to wear' },
+  { id: 2, title: 'Salt Water Taffy', description: 'The ocean is cool but what if it was candy?' },
+  { id: 3, title: 'Flying pigs', description: 'Would allow many things to finally come true' }
+];
+
+const addIdea = (idea) => {
+  let newIdeaList = ideas.concat([idea]);
+  setIdeas(newIdeaList);
+}
+
+const deleteIdea = (id) => {
+  let newIdeaList = ideas.fileter(idea => idea.id !== id);
+  setIdeas(newIdeaList);
+}
 
 const App = () => {
+  const [ideas, setIdeas] = useState(initialIdeas);
+
   return (
-    <h1>IdeaBox</h1>
+    <main className="App">
+      <h1>IdeaBox</h1>
+      <Ideas ideas={ideas}/>
+      <Form addIdea={addIdea} />
+    </main>
   )
 }
 
